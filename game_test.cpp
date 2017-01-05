6fdee6f34990927f9cf574e7df7bb124f40aa86c
CarmenIJsebaart <cijsebaart@gmail.com>
2017-01-05 21:04:25 +0100

    Added More Tests

--- game_test.cpp
+++ game_test.cpp
@@ -8,27 +8,6 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
 
-int f(const int i)
-{
-  if (i == 42) throw std::invalid_argument("Cansroigheh");
-  return i * 2;
-}
-
-BOOST_AUTO_TEST_CASE(test_mathematics_is_correct)
-{
-  BOOST_CHECK(1 + 1 == 2);
-
-  const double a{3.14};
-  const double b{3.1415};
-  BOOST_CHECK(a == a);
-  BOOST_CHECK_EQUAL(a, a);
-  BOOST_CHECK_CLOSE(a, b, 0.1);
-  BOOST_CHECK_NO_THROW(f(3));
-
-  BOOST_CHECK_THROW(f(42), std::invalid_argument);
-
-}
-
 BOOST_AUTO_TEST_CASE(test_game)
 {
   //Cannot be tested yet
--- vehicle_test.cpp
+++ vehicle_test.cpp
@@ -8,7 +8,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
 
-BOOST_AUTO_TEST_CASE(test_vehicle_set_and_get_should_be_symmetrical)
+BOOST_AUTO_TEST_CASE(test_vehicle_constuctor_arguments_should_be_gettable)
 {
   const sf::Color color(sf::Color::Green);
   const int length = 100;
@@ -25,7 +25,28 @@ BOOST_AUTO_TEST_CASE(test_vehicle_set_and_get_should_be_symmetrical)
   BOOST_CHECK(v.getDirection() == direction);
 }
 
+Vehicle create_test_vehicle() noexcept
+{
+  return Vehicle(sf::Vector2f(201.12, 156.15), 105, 45, sf::Color::Yellow, 5, true);
+}
+
+BOOST_AUTO_TEST_CASE(test_vehicle_setter_and_getter_must_be_symmetrical)
+{
+  Vehicle v = create_test_vehicle();
+  sf::Vector2f veh = v.getPosition();
+  veh.x += 1.0;
+  veh.y += 1.0;
+  v.setPosition(veh);
+  BOOST_CHECK(v.getPosition() == veh);
+
+  //Vehicle must be placed just outside window (on the left)
+  v.set_vehicle_left();
+  BOOST_CHECK(v.getPosition().x == -v.getLength());
+
+  //Vehicle must be placed just outside window (on the right)
+  const int window_width = 100;
+  v.set_vehicle_right(window_width);
+  BOOST_CHECK(v.getPosition().x == window_width);
+}
+
 #pragma GCC diagnostic pop
-
-
-
