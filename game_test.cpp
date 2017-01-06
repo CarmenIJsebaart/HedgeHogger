8d676dfea9332ec5bb7139038c9c927fb613d04a
CarmenIJsebaart <cijsebaart@gmail.com>
2017-01-06 12:29:13 +0100

    Added Hedgehog Move Test

--- game_test.cpp
+++ game_test.cpp
@@ -17,6 +17,38 @@ BOOST_AUTO_TEST_CASE(test_hedgehog_moves_up_when_user_presses_up_arrow)
   BOOST_CHECK(h_after.get_position() != h_before.get_position());
 }
 
+BOOST_AUTO_TEST_CASE(test_hedgehog_moves_down_when_user_presses_down_arrow)
+{
+  Game g;
+  g.press_key(sf::Keyboard::Up);
+  g.press_key(sf::Keyboard::Up);
+  const Hedgehog h_before = g.get_hedgehog();
+  g.press_key(sf::Keyboard::Down);
+  const Hedgehog h_after_down = g.get_hedgehog();
+  BOOST_CHECK(h_after_down.get_position().y > h_before.get_position().y);
+  BOOST_CHECK(h_after_down.get_position().x == h_before.get_position().x);
+}
+
+BOOST_AUTO_TEST_CASE(test_hedgehog_moves_right_when_user_presses_right_arrow)
+{
+  Game g;
+  const Hedgehog h_before = g.get_hedgehog();
+  g.press_key(sf::Keyboard::Right);
+  const Hedgehog h_after = g.get_hedgehog();
+  BOOST_CHECK(h_after.get_position().x > h_before.get_position().x);
+  BOOST_CHECK(h_after.get_position().y == h_before.get_position().y);
+}
+
+BOOST_AUTO_TEST_CASE(test_hedgehog_moves_left_when_user_presses_left_arrow)
+{
+  Game g;
+  const Hedgehog h_before = g.get_hedgehog();
+  g.press_key(sf::Keyboard::Left);
+  const Hedgehog h_after = g.get_hedgehog();
+  BOOST_CHECK(h_after.get_position().x < h_before.get_position().x);
+  BOOST_CHECK(h_after.get_position().y == h_before.get_position().y);
+}
+
 BOOST_AUTO_TEST_CASE(test_hedgehog_does_not_move_when_user_presses_down_arrow_at_start)
 {
   Game g;
